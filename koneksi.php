8b48fb65567a5ccf630cfde5ba9521c0ae2341b0
MhdWahyudi9 <mhd.wahyudi004@mhs.mdp.ac.id>
2023-07-03 18:25:40 +0700

    Setup DB Connection

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $db_host = "localhost";
+    $db_name = "id20992515_dbaplikasistasiunkai";
+    $db_username = "id20992515_yudi123";
+    $db_password = "Yudi123.";
+
+    $connect = mysqli_connect($db_host, $db_username, $db_password, $db_name) or die("Database is not connected");
